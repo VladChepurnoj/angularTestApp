9fb7e845ce3ec3b017868ec980540634d9b8f990
Vlad <chepurnojv@gmail.com>
2021-03-26 23:03:24 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {UserComponent} from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
