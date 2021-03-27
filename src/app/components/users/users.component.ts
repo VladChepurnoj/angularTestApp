53f2ceda0f059ebf7575883d0354a7078650645f
Vlad <chepurnojv@gmail.com>
2021-03-27 20:02:15 +0200

    added new user functionality, UI should be fixed later

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit,ViewChild } from '@angular/core';
 
 import {User} from '../../models/User'
 
@@ -8,10 +9,17 @@ import {User} from '../../models/User'
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  user:User={
+    firstName:'',
+    lastName:'',
+    email:''
+  }
   users: User[];
   showExtended:boolean=  true;
   loaded:boolean=false;
-  enableAdd:boolean=true
+  enableAdd:boolean=false;
+  showUserForm:boolean=false;
+  @ViewChild('userForm') form:any
 
   constructor() { }
 
@@ -21,47 +29,55 @@ export class UsersComponent implements OnInit {
         {
           firstName:"John",
           lastName:'Doe',
-          age:70,
-          address:{
-            street:'50 Main st',
-            city:'Boston',
-            state:'MA'
-          },
+          email:"john@gmail.com",
           isActive:true,
-          registered: new Date('01/02/2018 08:30:00')
+          registered: new Date('01/02/2018 08:30:00'),
+          hide:true
         },
         {
           firstName:"Kevin",
           lastName:'Johnson',
-          age:34,
-          address:{
-            street:'20 School st',
-            city:'Lynn',
-            state:'MA'
-          },
+          email:"kevin@gmail.com",
           isActive:false,
-          registered: new Date('03/11/2017 06:20:00')
+          registered: new Date('03/11/2017 06:20:00'),
+          hide:true
+
         },
         {
           firstName:"Karen",
           lastName:'Williams',
-          age:26,
-          address:{
-            street:'55 Mill st',
-            city:'Miami',
-            state:'FL'
-          },
+          email:"karen@gmail.com",
           isActive:true,
-          registered: new Date('11/02/2016 10:30:00')
+          registered: new Date('11/02/2016 10:30:00'),
+          hide:true
+
         }
       ]
   
       this.loaded=true
   }
 
-  addUser(user:User){
-    this.users.push(user)
+  // addUser(){
+  //   this.user.isActive=true
+  //   this.user.registered=new Date()
+  //   this.users.unshift(this.user)
+
+  //   this.user={
+  //     firstName:'',
+  //     lastName:'',
+  //     email:''
+  //   }
+  // }
+  onSubmit({value, valid}: {value:User, valid:boolean}){
+    if (!valid) {
+      console.log('Form is not valid')
+    } else {
+      value.isActive=true
+      value.registered=new Date()
+      value.hide=true
+      this.users.unshift(value)
+
+      this.form.reset()
+    }
   }
-
-
 }
